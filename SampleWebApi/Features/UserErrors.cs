ba88185d6076cc1d673a8f0a28c17c6b0ec04b2b
Edward <Edward@ED>
2024-01-02 10:11:51 +0800

    Add project files.

---
+++ SampleWebApi/Features/UserErrors.cs
@@ -0,0 +1,19 @@
+﻿using SampleWebApi.Common;
+
+namespace SampleWebApi.Features
+{
+    public class UserErrors
+    {
+        public static Error AlreadyExist(string fullname) =>
+            new Error("User.AlreadyExist", $"User {fullname} already exist");
+
+        public static Error NotFound() =>
+            new Error("User.NotFound", "User not found");
+
+        public static Error NoDataFound() =>
+            new Error("User.NoDataFound", "No Data found");
+
+        public static Error InputsNotMet() =>
+            new Error("Inputs.NotMet", "Conditions are not met please check your inputs");
+    }
+}
